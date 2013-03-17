ffe879bd787c2627eb58d484bb3348169be2002e
air_wang <28785383@qq.com>
2013-03-17 15:04:06 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HumDemo::Application.config.secret_token = '7aeea762da505b7bd5a963d96b5e07c7611a5d9debcaeeb70744145769997058cb864976d406268f730bdfdf3804761f95dca6c866496b433718405895dd569b'
